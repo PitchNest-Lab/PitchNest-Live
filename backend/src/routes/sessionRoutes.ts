4f0822ea560e368c5329c9dd692bf583d4ebbf46
Abhi Gurjar <abhishekbainsla190@gmail.com>
2026-05-30 22:01:57 +0530

    pitch summary creation api created

--- backend/src/routes/sessionRoutes.ts
+++ backend/src/routes/sessionRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware.ts";
-import { listSessions, getSession, deleteSession } from "../controllers/sessionController.ts";
+import { listSessions, getSession, deleteSession , createSession} from "../controllers/sessionController.ts";
 
 const router = Router();
 
 router.get("/", authMiddleware, listSessions);
 router.get("/:id", authMiddleware, getSession);
 router.delete("/:id", authMiddleware, deleteSession);
+router.post("/create", authMiddleware, createSession);
+
 
 export default router;
